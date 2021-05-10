5d66b5fbb98473ce8bd7ac4e02de664b0e252935
Hannah Gardner <hannahgardner@Hannahs-MBP.lan>
2021-05-10 14:58:30 -0500

    changes display of correct guesses to inline and alerts to incorrect geusses

--- Project1/project1b.ts
+++ Project1/project1b.ts
@@ -20,17 +20,17 @@
 // 2. User clicks a letter. If it is in the  word, the letter is revealed. If not, their incorrect guesses increase by one. && 3. The previously clicked letter's display changes and they cannot click it again. && 4. If their incorrect guesses reach 6, the game ends and they lose. && 5. If they guess all of the letters in the word, they win.
     // let incorrectGuesses = 0
     // let currentGuess"" = empty string variable that will store the guess 
-    // add event listeners to alphabet buttons (forEach loop?)
-    // onclick, change display to gray and change event listener to alert that they have already clicked (need more rsearch here) (3.)
-    // onclick, store innerText of button in currentGuess
+    // DONE add event listeners to alphabet buttons (forEach loop?)
+    // DONE onclick, change display to gray and change event listener to alert that they have already clicked (need more rsearch here) (3.)
+    // DONE onclick, store innerText of button in currentGuess
         // if secretWord.some(currentGuess) === false (2.)
-            // alert the letter is not in the word
+            // DONE  alert the letter is not in the word
             // increase incorrect guesses by one 
             // check if incorrect guesses is 6 (4.)
                 // if === 6, alert they lost
         // if secretWord.some(currentGuess) === true (2.)
-            // forEach LI, if innerText === currentGuess, change display from none to block
-            // forEach LI, if display === "block", alert player won, else alert to guess again (5.)
+            // DONE forEach LI, if innerText === currentGuess, change display from none to inline
+            // forEach LI, if display === "inline", alert player won, else alert to guess again (5.)
             // set curentGuess to an empty string
 
 // ===== INTERFACES ===== //
@@ -43,9 +43,9 @@ const secretWordUL: HTMLElement = document.querySelector("ul")
 
 let availableWords = ["snowflake", "icicle", "frosty", "cold", "frozen"]
 let secretWord = []
+let splitWordArr = []
 let availableLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 let incorrectGuesses = 0
-let currentGuess = ""
 
 // ===== FUNCTIONS ===== //
 // *** FUNCTIONS TO SET UP SECRET WORD *** //
@@ -58,36 +58,76 @@ const selectSecretWord = () => {
 
 const splitWord = () => {
     let arrayToSplit = selectSecretWord()
-    let splitWordArr = arrayToSplit[0].toUpperCase().split("")
-    return splitWordArr
+    splitWordArr = arrayToSplit[0].toUpperCase().split("")
 }
 
 const inputSecretWordLI = () => {
-    let arrayToAppend = splitWord()
-    arrayToAppend.forEach(letter => {
+    splitWordArr.forEach(letter => {
       let newLetter = document.createElement("li")
       newLetter.innerText = letter
-      newLetter.setAttribute("id", `${letter}`)
+      newLetter.setAttribute("id", `hidden${letter}`)
       newLetter.setAttribute("class", "hidden-letter")
       secretWordUL.appendChild(newLetter)
     })
 }
 
 // *** FUNCTIONS TO RESPOND TO PLAYER GUESSES *** //
-// const changeLetterDisplay = (e) => {
-//     let letter = e.currentTarget as HTMLButtonElement
-//     letter.style.backgroundColor = "gray"
+const recordGuess = (e) => {
+    let letter = e.currentTarget as HTMLButtonElement
+    return letter
+}
+
+const changeLetterDisplay = (element) => {
+    element.style.backgroundColor = "gray"
+    element.removeEventListener("click", letterListener)
+}
+
+const alertLetterUnavailable = (element) => {
+    element.addEventListener("click", () => {
+        alert("You have already used this letter.")
+    })
+}
+
+const compareCurrentGuess = (element) => {
+    return (splitWordArr.some(letter => letter === element.id))
+}
+
+const increaseIncorrectGuesses = () => {
+    incorrectGuesses = incorrectGuesses + 1
+}
+
+// const checkLIDisplay = (arr: []) => {
+//     let 
 // }
+
+const playerGuesses = (e) => {
+    let letter = recordGuess(e)
+    changeLetterDisplay(letter)
+    alertLetterUnavailable(letter)
+    let lettersToReveal = document.querySelectorAll(`#hidden${letter.id}`)
+    if (compareCurrentGuess(letter)) {
+        lettersToReveal.forEach(x => {
+            x.style.display = "inline"
+        })
+    } else if (compareCurrentGuess(letter) === false) {
+        alert("Oops! That letter is not in the secret word.")
+        increaseIncorrectGuesses
+    // } else if () {
+    //     alert("You guessed the word in time. You win!")
+    }
+}
 
 // ===== EVENT LISTENERS ===== //
 const startButtonListener = () => {
+    selectSecretWord()
+    splitWord()
     inputSecretWordLI()
 }
 
 startButton.addEventListener("click", startButtonListener)
 
 const letterListener = (e) => {
-    // changeLetterDisplay(e)
+    playerGuesses(e)
 }
 
 availableLetters.forEach(letter => {
